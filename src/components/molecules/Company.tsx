fc1c4dd1062b2d22f7bffec16d9d44dd233bb0a4
Kpaubert <43110067+Kpaubert@users.noreply.github.com>
2020-12-12 23:56:39 +0100

    Update src/components/molecules/Company.tsx
    Co-authored-by: Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>

--- src/components/molecules/Company.tsx
+++ src/components/molecules/Company.tsx
@@ -10,7 +10,7 @@ const Company: React.FC<Props> = (props) => {
         <Link to="#">
             <article className="p-4 m-2 rounded-xl dark:bg-black dark:text-white">
                 <h1 className="text-3xl font-bold mb-0">{props.company.name}</h1>
-                <div className={`bg-gradient-to-r ${company.gradient} h-2 rounded-full my-1`}></div>
+                <div className={`bg-gradient-to-r ${props.company.gradient} h-2 rounded-full my-1`}></div>
                 <small className="dark:text-gray-300 text-sm mb-3">
                     {numPresentations > 0
                         ? numPresentations + ' kommende arrangementer'

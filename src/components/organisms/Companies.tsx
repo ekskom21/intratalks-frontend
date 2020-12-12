fdeb21caa699538ee7217b1d4c7c2c3ab1fd332f
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-12 20:43:32 +0100

    Add companies page

---
+++ src/components/organisms/Companies.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Companies: React.FC = () => {
+    const companies = [
+        { name: 'Bekk' },
+        // Fill out more relevant information regarding the company as you see fit.
+    ];
+
+    return <h1>Companies!</h1>;
+};
+
+export default Companies;
--- src/components/organisms/Router.tsx
+++ src/components/organisms/Router.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import { Switch, Route } from 'react-router-dom';
+
+import Companies from './Companies';
 import Frontpage from './Frontpage';
 
 const Router: React.FC = () => {
     return (
         <Switch>
             <Route exact path="/" component={Frontpage} />
+            <Route exact path="/companies" component={Companies} />
         </Switch>
     );
 };

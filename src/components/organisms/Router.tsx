26b224f22aacb84f00256473eafe282b1691d228
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-12 23:23:46 +0100

    Add event detail page

--- src/components/organisms/Router.tsx
+++ src/components/organisms/Router.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ComingEvents from './ComingEvents';
+import EventDetails from './EventDetails';
 
 const Router: React.FC = () => {
     return (
-        <Switch>
-            <Route exact path="/" component={ComingEvents} />
-        </Switch>
+        <div className="dark:bg-black dark:text-white min-h-screen">
+            <Switch>
+                <Route exact path="/" component={ComingEvents} />
+                <Route path="/event/:id" component={EventDetails} />
+            </Switch>
+        </div>
     );
 };
 

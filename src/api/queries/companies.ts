f8685dae565f3dca571ca261c6b2a44f0799c33f
AndreasWintherMoen <andreas@winthermoen.no>
2020-12-13 20:02:46 +0100

    Add query for specific company details

--- src/api/queries/companies.ts
+++ src/api/queries/companies.ts
@@ -14,6 +14,26 @@ export const ALL_COMPANIES = gql`
     }
 `;
 
+export const COMPANY = gql`
+    query($_id: ID!) {
+        company(_id: $_id) {
+            name
+            colors {
+                primary
+                secondary
+            }
+            description
+            events {
+                _id
+            }
+        }
+    }
+`;
+
 export type AllCompanies = {
     companies: Array<Pick<Company, '_id' | 'name' | 'colors'>>;
+};
+
+export type SpecificCompany = {
+    company: Pick<Company, 'name' | 'colors' | 'description' | 'events'>;
 };

0c973c973101a6700cf52db25e0de03f1689911d
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-13 11:09:05 +0100

    Remove frontpage route

--- src/components/organisms/Router.tsx
+++ src/components/organisms/Router.tsx
@@ -5,15 +5,13 @@ import { Switch, Route } from 'react-router-dom';
 import ComingEvents from './ComingEvents';
 import EventDetails from './EventDetails';
 import Companies from './Companies';
-import Frontpage from './Frontpage';
 
 const Router: React.FC = () => {
     return (
         <div className="dark:bg-black dark:text-white min-h-screen">
             <Switch>
-                <Route exact path="/" component={Frontpage} />
-                <Route exact path="/companies" component={Companies} />
                 <Route exact path="/" component={ComingEvents} />
+                <Route exact path="/companies" component={Companies} />
                 <Route path="/event/:id" component={EventDetails} />
             </Switch>
         </div>

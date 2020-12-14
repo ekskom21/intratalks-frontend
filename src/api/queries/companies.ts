02acf9a92a7b6e5316290db3aef29bf7862bfc68
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-14 12:13:15 +0100

    Clean up event and company fetching

--- src/api/queries/companies.ts
+++ src/api/queries/companies.ts
@@ -5,7 +5,17 @@ export const ALL_COMPANIES = gql`
     query {
         companies {
             _id
-            name
+            events {
+                _id
+                title
+                time
+                location {
+                    lat
+                    lng
+                    name
+                }
+                description
+            }
             colors {
                 primary
                 secondary
@@ -15,5 +25,5 @@ export const ALL_COMPANIES = gql`
 `;
 
 export type AllCompanies = {
-    companies: Array<Pick<Company, '_id' | 'name' | 'colors'>>;
+    companies: Array<Company>;
 };
--- src/components/atoms/SmallEvent.tsx
+++ src/components/atoms/SmallEvent.tsx
@@ -3,24 +3,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import classNames from 'classnames';
-import { EventWithCompany } from '../../generated/graphql';
+import { Company, Event } from '../../generated/graphql';
 
-const SmallEvent: React.FC<{ event: EventWithCompany }> = ({ event }) => {
+type Props = {
+    event: Event;
+    company: Company;
+};
+
+const SmallEvent: React.FC<Props> = ({ company, event }) => {
     return (
-        <Link to={`/event/${event.event._id}`}>
+        <Link to={`/event/${event._id}`}>
             <section className={classNames('flex', 'justify-between', 'my-2')}>
                 <div>
-                    <h5 className={classNames('font-semibold')}>{event.event.title}</h5>
+                    <h5 className={classNames('font-semibold')}>{event.title}</h5>
                     <small>
-                        {event.company.name} · {event.event.location.name}
+                        {company.name} · {event.location.name}
                     </small>
                 </div>
                 <span
                     className={classNames(
                         'self-center',
                         'bg-gradient-to-r',
-                        `from-${event.company.colors.primary}`,
-                        `to-${event.company.colors.secondary}`,
+                        `from-${company.colors.primary}`,
+                        `to-${company.colors.secondary}`,
                         'font-extrabold',
                         'text-xl',
                         'mb-2',

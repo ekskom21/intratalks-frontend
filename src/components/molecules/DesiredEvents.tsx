42cf326b389cc8c4cb699472c5bc8da6aad5bb73
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-01-02 21:31:09 +0100

    See your previously selected desired events

--- src/components/molecules/DesiredEvents.tsx
+++ src/components/molecules/DesiredEvents.tsx
@@ -5,10 +5,16 @@ import classNames from 'classnames';
 import { useQuery } from '@apollo/client';
 
 import { AllCompanies, ALL_COMPANIES } from '../../api/queries/companies';
+import { DesiredEvents as DesiredEventsT, DESIRED_EVENTS } from '../../api/queries/desiredEvents';
 import { Event } from '../../generated/graphql';
 
 const DesiredEvents: React.FC = () => {
     const { data, loading, error } = useQuery<AllCompanies>(ALL_COMPANIES);
+    const {
+        data: desiredEventsData,
+        loading: desiredEventsLoading,
+        error: desiredEventsError,
+    } = useQuery<DesiredEventsT>(DESIRED_EVENTS);
 
     const events = React.useMemo(() => {
         const baseMap = {
@@ -28,6 +34,10 @@ const DesiredEvents: React.FC = () => {
         return <p className="mt-4">Laster arrangementer…</p>;
     }
 
+    if (desiredEventsLoading) {
+        return <p className="mt-4">Laster dine valgte arrangementer…</p>;
+    }
+
     if (error) {
         return (
             <p className="mt-4">
@@ -36,22 +46,53 @@ const DesiredEvents: React.FC = () => {
         );
     }
 
+    if (desiredEventsError) {
+        return (
+            <p className="mt-4">
+                Kunne ikke laste dine ønskede arrangementer: {desiredEventsError.message} Vennligst forsøk å laste inn
+                siden på nytt.
+            </p>
+        );
+    }
+
+    const breakfast = desiredEventsData?.desiredEvents.breakfast;
+    const lunch = desiredEventsData?.desiredEvents.lunch;
+    const dinner = desiredEventsData?.desiredEvents.dinner;
+
     return (
         <>
             <h2 className={classNames('text-2xl', 'font-bold', 'mt-4')}>Ønsker</h2>
             <small>Ønskene dine blir automatisk lagret.</small>
 
             <Select
+                defaultValue={
+                    breakfast
+                        ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          { label: events.BREAKFAST.find((e) => e._id === breakfast)!.title, value: breakfast }
+                        : null
+                }
                 className={classNames('text-black', 'mt-2')}
                 placeholder="Velg et frokostarrangement"
                 options={events.BREAKFAST.map((event) => ({ label: event.title, value: event._id }))}
             />
             <Select
+                defaultValue={
+                    lunch
+                        ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          { label: events.LUNCH.find((e) => e._id === lunch)!.title, value: lunch }
+                        : null
+                }
                 className={classNames('text-black', 'mt-4')}
                 placeholder="Velg et lunsjarrangement"
                 options={events.LUNCH.map((event) => ({ label: event.title, value: event._id }))}
             />
             <Select
+                defaultValue={
+                    dinner
+                        ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          { label: events.DINNER.find((e) => e._id === dinner)!.title, value: dinner }
+                        : null
+                }
                 className={classNames('text-black', 'mt-4')}
                 placeholder="Velg et middagsarrangement"
                 options={events.DINNER.map((event) => ({ label: event.title, value: event._id }))}

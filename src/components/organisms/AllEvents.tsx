178e501fe5e4085e0263ec148cd73aedd2b36b5d
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2021-01-03 18:34:21 +0100

    Update src/components/organisms/AllEvents.tsx
    Co-authored-by: Jonathan Brooks <brooks.jonathan1@gmail.com>

--- src/components/organisms/AllEvents.tsx
+++ src/components/organisms/AllEvents.tsx
@@ -28,7 +28,7 @@ const AllEvents: React.FC = () => {
                             <Link className="text-blue-500 dark:text-blue-400" to="/profile">
                                 gå til profil-siden
                             </Link>{' '}
-                            og velge fra rullgardinmenyene der.
+                            og velge fra rullegardinmenyene der.
                         </p>
                     ) : (
                         <p>

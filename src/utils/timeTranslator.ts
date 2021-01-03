3992caa540a0375bc17a46014219c3428b19ea18
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-01-03 15:21:58 +0100

    Add some helpers for doing time calculations

---
+++ src/utils/timeTranslator.ts
@@ -0,0 +1,27 @@
+import { EventTime } from '../generated/graphql';
+import { addHours, fromUnixTime } from 'date-fns';
+
+export const BASE_TIME = fromUnixTime(1613642400);
+
+export const translateTime = (time: EventTime): Date => {
+    return {
+        [EventTime.Breakfast]: BASE_TIME,
+        [EventTime.Lunch]: addHours(BASE_TIME, 4),
+        [EventTime.Dinner]: addHours(BASE_TIME, 9),
+    }[time];
+};
+
+export const timetable: Record<EventTime, { start: Date; end: Date }> = {
+    BREAKFAST: {
+        start: translateTime(EventTime.Breakfast),
+        end: addHours(translateTime(EventTime.Breakfast), 2),
+    },
+    LUNCH: {
+        start: translateTime(EventTime.Lunch),
+        end: addHours(translateTime(EventTime.Lunch), 3),
+    },
+    DINNER: {
+        start: translateTime(EventTime.Dinner),
+        end: addHours(translateTime(EventTime.Dinner), 5),
+    },
+};
